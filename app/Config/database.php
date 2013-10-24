10fa67507b6d3701434ca63377ab44c3e4feff41
64themes <64themes@1fdb6ce9-dc21-4d98-8a29-15cbca98af3a>
2013-10-24 18:53:32 +0000

    db fix

--- app/Config/database.php
+++ app/Config/database.php
@@ -59,7 +59,7 @@ class DATABASE_CONFIG {
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
 		'host' => 'localhost',
-		'login' => 'root',
+		'login' => 'shamone',
 		'password' => 'fiosof734%&&rell',
 		'database' => 'firepaperdev',
 		'prefix' => '',
