5b14f8324c12a508762d21f633a998bbe4df44a5
sourabgupta88 <sourabgupta88@1fdb6ce9-dc21-4d98-8a29-15cbca98af3a>
2013-11-02 17:58:11 +0000

    In order to solve undefined error

---
+++ app/Model/fileType.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This clas file will be used for the project's task table related modules
+ * Specfically used in the projects controller
+ */
+class fileType extends AppModel {
+	   var $name = 'fileType';
+	   var $useTable= "file_types";
+	   var $errMsg=array();
+	   var $err=0; 
+	  
+}
+?>
