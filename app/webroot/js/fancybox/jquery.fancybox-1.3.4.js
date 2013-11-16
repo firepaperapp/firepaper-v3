e1795a7b84fa76837f7bc9210540488fce5d7a8e
64themes <64themes@1fdb6ce9-dc21-4d98-8a29-15cbca98af3a>
2013-11-16 16:42:34 +0000

    Invite

--- app/webroot/js/fancybox/jquery.fancybox-1.3.4.js
+++ app/webroot/js/fancybox/jquery.fancybox-1.3.4.js
@@ -1045,7 +1045,7 @@
 		);
 
 		outer = $('<div id="fancybox-outer"></div>')
-			.append('<div class="fancybox-bg" id="fancybox-bg-n"></div><div class="fancybox-bg" id="fancybox-bg-ne"></div><div class="fancybox-bg" id="fancybox-bg-e"></div><div class="fancybox-bg" id="fancybox-bg-se"></div><div class="fancybox-bg" id="fancybox-bg-s"></div><div class="fancybox-bg" id="fancybox-bg-sw"></div><div class="fancybox-bg" id="fancybox-bg-w"></div><div class="fancybox-bg" id="fancybox-bg-nw"></div>')
+			.append('')
 			.appendTo( wrap );
 
 		outer.append(
