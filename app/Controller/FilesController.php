99ad011e088475f5dfcaa3c5933b30fb0d983178
amandudi <amandudi@1fdb6ce9-dc21-4d98-8a29-15cbca98af3a>
2013-11-18 16:33:27 +0000

    1. Debuging

--- app/Controller/FilesController.php
+++ app/Controller/FilesController.php
@@ -49,9 +49,9 @@ class FilesController  extends AppController{
 	function getFiles($id=0)
 	{ 
 		if($this->Session->read("admin_id") != 0){
-			die('212');
 			$cur_id = $this->Session->read("admin_id");
 		}else{
+			die('212');
 			$cur_id = $this->Session->read("userid");
 		}
 
